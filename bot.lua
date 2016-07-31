92ae2228a61181e784e5beb14f5b9b9cb786875d
aminkavarii <amin.kavery.nasab.0631133@gmail.com>
2016-07-31 10:56:47 +0000

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -16,7 +16,7 @@ local BASE_FOLDER = ""
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {97242093, pika launcher id}-- put your id and cli pika launcher bot
+  local admins = {97242093,225473605}-- put your id and cli pika launcher bot
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
